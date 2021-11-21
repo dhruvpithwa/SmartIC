13278cb71b2e628ca2262ef7b212b16bdc78ac63
Dhruv Pithwa <dhruvpithwa1999@gmail.com>
2021-11-21 19:32:39 +0530

    backend connected

--- home.js
+++ home.js
@@ -23,21 +23,22 @@ function getBookings(filterObj){
                         
                         <div class="col-md-2 card m-4">
                             <div class="row">
-
-                                <div class="m-4">
+                                <div class="mt-3 ml-3 mr-3">
                                     <img class="card-img-top product_img" src="./sample.jpg" alt="Card image">
                                 </div>    
                                 <div class="card-body">
                                     <h5 class="card-title capitalize">${(obj.name)}</h5>
-                                    <h5 class="card-subtitle text-muted">₹ ${obj.price}</h5>
+                                    <h5 class="card-subtitle text-muted">Rs ${obj.price}</h5>
                                     <br>
-                                <a class="card-subtitle" href="/${(obj.id)}">Read more</a>
-                                </div>    
-                            </div> 
+                                    <h6 class="card-subtitle">Description</h6>
+                                    <p>${obj.description}</p> 
+                                </div>
+                            </div>
                         </div>
                         
                         `;
                         productHTML = productHTML.concat(text)
+                        
 
                     })
 
